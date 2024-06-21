3752a72ad9a76200b59729ee3a998fab4c7ab9ad
Nauffal Nur Rizky <nauffal2005@gmail.com>
2024-06-21 13:48:16 +0700

    210624

--- app.js
+++ app.js
@@ -3,9 +3,45 @@ document.addEventListener('DOMContentLoaded', () => {
   pawns.forEach((pawn) => {
     window.addEventListener('click', (e) => {
       if (e.target === pawn) {
-        console.log(e.target.querySelector('i'));
+        const element = e.target.querySelector('i');
+        if (!element) return false;
+
+        const classString = element.classList[1];
+
+        if (classString.includes('pawn')) {
+          console.log(`It's chess pawn!`);
+
+          // MAKE THE FOOTSTEP OF THE MOVEMENTS
+        } else if (classString.includes('knight')) {
+          console.log(`It's chess knight!`);
+        } else if (classString.includes('rook')) {
+          console.log(`It's chess rook!`);
+        } else if (classString.includes('bishop')) {
+          console.log(`It's chess bishop!`);
+        } else if (classString.includes('queen')) {
+          console.log(`It's chess queen!`);
+        } else if (classString.includes('king')) {
+          console.log(`It's chess king!`);
+        }
       } else if (e.target === pawn.querySelector('i')) {
-        console.log(e.target);
+        const element = e.target;
+        const classString = element.classList[1];
+
+        if (classString.includes('pawn')) {
+          console.log(`It's chess pawn!`);
+
+          // MAKE THE FOOTSTEP OF THE MOVEMENTS
+        } else if (classString.includes('knight')) {
+          console.log(`It's chess knight!`);
+        } else if (classString.includes('rook')) {
+          console.log(`It's chess rook!`);
+        } else if (classString.includes('bishop')) {
+          console.log(`It's chess bishop!`);
+        } else if (classString.includes('queen')) {
+          console.log(`It's chess queen!`);
+        } else if (classString.includes('king')) {
+          console.log(`It's chess king!`);
+        }
       }
     });
   });
